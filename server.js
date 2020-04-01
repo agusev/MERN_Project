6d415ce612ed55dc7cc0e3f08915618d8ad34099
agusev <1990agusev@gmail.com>
2020-04-01 01:05:31 -0700

    day 02

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const app = express();
 connectDB();
 
 // Init Middleware
-app.use(express.json());
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('API is running'));
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -16,16 +18,18 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+// // Serve static assets in production
+// if (process.env.NODE_ENV === 'production') {
+//     // Set static folder
+//     app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     });
+// }
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () =>
+    console.log(`Server started on port ${PORT}`)
+);
