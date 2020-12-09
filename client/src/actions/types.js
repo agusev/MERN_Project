142cae0f8846cdb6eb583cf55d19fead244ef832
agusev <1990agusev@gmail.com>
2020-12-10 01:10:51 +0300

    new

---
+++ client/src/actions/types.js
@@ -0,0 +1,8 @@
+export const SET_ALERT = 'SET ALERT';
+export const REMOVE_ALERT = 'REMOVE ALERT';
+export const REGISTER_SUCCESS = 'REGISTER SUCCESS';
+export const REGISTER_FAIL = 'REGISTER FAIL';
+export const USER_LOADED = 'USER LOADED';
+export const AUTH_ERROR = 'AUTH ERROR';
+export const LOGIN_SUCCESS = 'LOGIN SUCCESS';
+export const LOGIN_FAIL = 'LOGIN FAIL';
