8326488392231b02403ab55a913aec80a0bb46a3
agusev <1990agusev@gmail.com>
2020-12-19 23:55:43 +0300

    Dec 19 2020

---
+++ client/src/App.js
@@ -0,0 +1,59 @@
+import React, { Fragment, useEffect } from 'react';
+import './App.css';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
+
+//Redux
+// to connect with redux
+import { Provider } from 'react-redux';
+
+import store from './store';
+
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+import { LOGOUT } from './actions/types';
+
+if (localStorage.token) {
+	setAuthToken(localStorage.token);
+}
+
+const App = () => {
+	useEffect(() => {
+		// check for token in LS
+		if (localStorage.token) {
+			setAuthToken(localStorage.token);
+		}
+		if (localStorage.token) store.dispatch(loadUser());
+
+		// log user out from all tabs if they log out in one tab
+		// window.addEventListener('storage', () => {
+		// 	if (!localStorage.token) store.dispatch({ type: LOGOUT });
+		// });
+	}, []);
+
+	return (
+		<Provider store={store}>
+			<Router>
+				<Fragment>
+					<Navbar />
+					<Route exact path='/' component={Landing} />
+					<section className='container'>
+						<Alert />
+						<Switch>
+							<Route exact path='/register' component={Register} />
+							<Route exact path='/login' component={Login} />
+						</Switch>
+					</section>
+				</Fragment>
+			</Router>
+		</Provider>
+	);
+};
+
+export default App;
